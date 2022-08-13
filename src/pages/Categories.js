f59b7d6705fe822de7b9f72bcdbda2198eb45be7
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-13 04:15:52 -0300

    Add pages

---
+++ src/pages/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CheckStatusButton from '../components/CheckStatusButton';
+
+const Categories = () => (
+  <>
+    <CheckStatusButton />
+  </>
+);
+
+export default Categories;
