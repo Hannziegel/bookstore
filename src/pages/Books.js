8b6bfb91728579747a29170c1c2bebd69467ad2b
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-20 16:28:57 -0300

    Update index and pages

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BooksList from '../components/BookList';
 
-const Books = () => (
-  <>
-    <BooksList />
-  </>
-);
+const Books = () => {
+  const BookList = useSelector((state) => state.book);
+
+  return (
+    <>
+      <BooksList bookList={BookList} />
+    </>
+  );
+};
 
 export default Books;
