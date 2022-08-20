3e054f13f77377987df00ac658c585bde1fff1ff
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-08-20 19:09:08 -0300

    Update redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,19 +1,10 @@
 const ADD = 'bookstore/books/ADD_BOOK';
 const REMOVE = 'bookstore/books/REMOVE_BOOK';
+const GET = 'bookstore/books/GET_BOOK';
+const API_ID = 'DRrhhx0Pgnk6pSVsDI0Z';
 
 // initial state
-const initialState = [
-  {
-    title: 'The name of the wind',
-    author: 'Patrick Rothfuss',
-    id: '1',
-  },
-  {
-    title: 'American Gods',
-    author: 'Neil Gaiman',
-    id: '2',
-  },
-];
+const initialState = [];
 
 // reducer
 const bookReducer = (state = initialState, action) => {
@@ -21,7 +12,9 @@ const bookReducer = (state = initialState, action) => {
     case ADD:
       return [...state, action.book];
     case REMOVE:
-      return state.filter((book) => book.id !== action.book.id);
+      return state.filter((book) => book.item_id !== action.book.item_id);
+    case GET:
+      return action.payload;
     default:
       return state;
   }
@@ -31,9 +24,45 @@ export default bookReducer;
 
 // action creators
 
-export const addBook = (book) => ({
-  type: ADD,
-  book,
-});
+export const addBook = (book) => async (dispatch) => {
+  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'aplication/json',
+    },
+    body: JSON.stringify({
+      item_id: book.item_id,
+      title: book.title,
+      author: book.author,
+      category: book.category,
+    }),
+  }).then(dispatch({
+    type: ADD,
+    book,
+  }));
+};
 
-export const removeBook = (book) => ({ type: REMOVE, book });
+export const removeBook = (book) => async (dispatch) => {
+  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books/${book.item_id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(dispatch({
+    type: REMOVE, book,
+  }));
+};
+
+export const getBooks = () => async (dispatch) => {
+  const books = [];
+  const fetchBooks = await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books`).then((data) => data.json());
+
+  Object.keys(fetchBooks).forEach((e) => {
+    books.push({ ...fetchBooks[e][0], item_id: e });
+  });
+
+  dispatch({
+    type: GET,
+    payload: books,
+  });
+};
